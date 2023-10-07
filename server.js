e991f46a3b28230924681418720b87ae5dac4056
david <david.emanoel.706@ufrn.edu.br>
2023-10-07 20:43:21 -0300

    database connect

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
-const http = require('http')
-const app = require('./app')
+require('dotenv').config()
+const mongoUrl = process.env.DATA_BASE
+
+
+const mongoose = require('mongoose')
+const express = require('express')
+
+const userRoutes = require('./api/routes/users')
+const passRoutes = require('./api/routes/pass')
+
+// const app = require('./app')
 
 const port = process.env.PORT || 3000
 
-const server = http.createServer(app)
+// const server = http.createServer(app)
 
-server.listen(port)
+mongoose.connect(mongoUrl)
+const database= mongoose.connection
+
+database.on('connected', () => console.log('Database connected'))
+
+const app = express()
+
+app.use(express.json())
+
+// Routes which should handle requests
+app.use('/users', userRoutes)
+app.use('/pass', passRoutes)
+
+app.listen(port, () => {
+    console.log(`Server started at ${3000}`)
+})
