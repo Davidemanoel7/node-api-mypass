fc1dd1b33dc202e01ee493fedfae6653d4e44191
david <david.emanoel.706@ufrn.edu.br>
2023-10-07 11:57:20 -0300

    update

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
-// const mongoose = require('mongoose')
 
 const userRoutes = require('./api/routes/users')
 const passRoutes = require('./api/routes/pass')
@@ -33,14 +32,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
-  //connecting to mongoDB with mongoose
-// mongoose.connect(
-//     "mongodb+srv://AdminMyPass:"+process.env.MONGO_ATLAS_PASS+"@node-rest-mypass.pvhuw2o.mongodb.net/?retryWrites=true&w=majority",
-//     // {
-//     //     useMongoClient: true
-//     // }
-//     )
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded( {extended: false} ))
@@ -84,12 +75,3 @@ app.use((error, req, res, next) => {
 })
 
 module.exports = app
-
-//https://youtube.com/playlist?list=PL55RiY5tL51q4D-B63KBnygU6opNPFk_q
-
-// https://youtu.be/WDrU305J1yw?si=z3oP8rA3QnuKX3FR&t=740
-
-
-
-// FALTA CONECTAR AO BANCO DE DADOS
-// https://cloud.mongodb.com/v2/6519d83342a4ea21b3725ac5#/clusters/connect?clusterId=node-rest-mypass
