cefd307c09e19c355c008a2e83a98606d1dcce68
david <david.emanoel.706@ufrn.edu.br>
2023-10-07 20:43:10 -0300

    clear app file

--- app.js
+++ app.js
@@ -3,36 +3,6 @@ const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
-
-const userRoutes = require('./api/routes/users')
-const passRoutes = require('./api/routes/pass')
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://AdminMyPass:" + process.env.MONGO_ATLAS_PASS + "@node-rest-mypass.pvhuw2o.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-});
-
-async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-}
-run().catch(console.dir);
-
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded( {extended: false} ))
 app.use(bodyParser.json())
@@ -54,9 +24,6 @@ app.use((req, res, next) => {
     next()
 })
 
-// Routes which should handle requests
-app.use('/users', userRoutes)
-app.use('/pass', passRoutes)
 
 app.use((req, res, next) => {
     const error = new Error('Not found :( ')
