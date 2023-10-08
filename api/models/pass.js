643a9afe91c832025cb2fb95c11f898a9fd6e906
david <david.emanoel.706@ufrn.edu.br>
2023-10-07 22:48:17 -0300

    update data schema

--- api/models/pass.js
+++ api/models/pass.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
+const User = require('../models/users');
 
 const passSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    description: String,
-    password: String,
-    userId: mongoose.Schema.Types.ObjectId
+    description: {
+        required: false,
+        type: String
+    },
+    password: {
+        required: true,
+        type: String
+    },
+    // userId deve ser mesmo string??
+    userId: {
+        type: String,
+        ref: User
+    }
 })
+
+module.exports = mongoose.model('Pass', passSchema)
