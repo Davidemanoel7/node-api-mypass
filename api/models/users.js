df99e220cc2a207a49048c428f705e159c767b2f
david <david.emanoel.706@ufrn.edu.br>
2023-10-07 22:47:50 -0300

    update data schema

--- api/models/users.js
+++ api/models/users.js
@@ -2,10 +2,22 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    user: String,
-    email: String,
-    password: String,
+    name: {
+        required: true,
+        type: String,
+    },
+    user: {
+        required: true,
+        type: String,
+    },
+    email: {
+        required: true,
+        type: String,
+    },
+    password: {
+        required: true,
+        type: String
+    },
 })
 
 module.exports = mongoose.model('User', userSchema)
