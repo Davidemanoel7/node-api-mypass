73893e58337da07711f2ac52d87b019cf01715c9
david <david.emanoel.706@ufrn.edu.br>
2023-10-07 22:48:44 -0300

    fix: routes

--- api/routes/pass.js
+++ api/routes/pass.js
@@ -2,37 +2,91 @@ const express = require('express')
 
 const router = express.Router()
 
+const mongoose = require('mongoose')
+
+const Pass = require('../models/pass')
+const User = require('../models/users')
+
 //não usar /pass, pois em app.js já é referenciado.
 // caso use, o end-point seria: /pass/pass/
+//
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        massage: 'Request(GET) to /pass/'
-    })    
+    Pass.find()
+        .then( docs => {
+            console.log(docs)
+            res.status(200).json(docs)    
+        })
+        .catch( err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
 router.post('/', (req, res, next) => {
-    const pass = {
-        productId: req.body.productId,
-        quantity: req.body.quantity,
-    }
-    res.status(201).json({
-        massage: 'Request(POST) to /pass/',
-        createdpass: pass
+    
+    const pass = new Pass({
+        _id: new mongoose.Types.ObjectId(),
+        description: req.body.description,
+        password: req.body.password,
+        userId: req.body.userId
     })
+
+    pass.save()
+        .then( result => {
+            console.log(result)
+            res.status(201).json({
+                message: 'Request (POST) to /pass sucessfully',
+                createdpass: pass
+            })
+        })
+        .catch(
+            err => {
+                console.log(err)
+                res.status(500).json({
+                    error: err
+                })
+            }
+        )
 })
 
-router.get('/:passId', (req, res, next) => {
-    res.status(200).json({
-        massage: 'Request(GET) to /pass/id/',
-        passId: req.params.passId,
-    })
+// GET all passwords with userId
+router.get('/:userId', (req, res, next) => {
+    const id = req.params.userId
+    
+    Pass.findById({userId: id})
+        .exec()
+        .then( doc => {
+            console.log('From mongoDB', doc)
+            if ( doc ) {
+                res.status(200).json( doc )
+            } else {
+                res.status(404).json({
+                message: "Not found or invalid entry for provided ID"
+                })
+            }
+        })
+        .catch( err => {
+            console.log(err)
+            res.status(500).json({error: err})
+        })
 })
 
 router.delete('/:passId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Request(DELETE) to /pass/id',
-        passId: req.params.passId,
-    })
+    const id = req.params.passId
+
+    Pass.findByIdAndDelete({_id: id})
+        .exec()
+        .then( result => {
+            res.status(200).json(result)
+        })
+        .catch( err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
 module.exports = router
