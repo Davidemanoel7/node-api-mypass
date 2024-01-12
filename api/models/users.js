4ab3cbb482cc23a35f2621a8077b4c9b9f5fb2ec
david <david.emanoel.706@ufrn.edu.br>
2024-01-12 20:35:00 -0300

    update model adding living prop to pseudo-delete function

--- api/models/users.js
+++ api/models/users.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
     profileImage: {
         required: false,
         type: String
+    },
+    living: {
+        type: Boolean,
+        default: true
     }
 });
 
