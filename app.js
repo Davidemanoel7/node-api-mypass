a9d9e7a8be31d34fca3a356f8ee8f8896ff1836b
david <david.emanoel.706@ufrn.edu.br>
2024-01-13 18:19:37 -0300

    Some updates and new package: cors

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded( {extended: false} ))
-app.use(bodyParser.json())
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded( {extended: false} ));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use((req, res, next) => {
-    // Restringir o acesso da API
+    // Restringir o acesso da API (nesse caso qualquer origem é aceita). '*' deverá ser substituído pela url da aplicação pós deploy.
     res.header('Access-Control-Allow-Origin', '*')
     
     // Tratar erros de CORS
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     
-    // 
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-        return res.status(200).json({
-
-        })
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
     }
-    next()
-})
+    next();
+});
 
 
 app.use((req, res, next) => {
