d49df412a8aa18bd361145bb40314af473461ffb
david <david.emanoel.706@ufrn.edu.br>
2024-01-16 12:38:26 -0300

    added: checkAuth function for userTypes

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,15 +1,37 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(" ")[1]
-        // console.log(`token headers middleware: ${token}`)
-        const decoded = jwt.verify(token, process.env.JWT_KEY)
-        req.userData = decoded;
-        next();
-    } catch (error) {
-        res.status(401).json({
-            message: 'Auth failed...'
-        })
+const checkAuth = (userType) => {
+    return ( req, res, next ) => {
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+
+            if ( !token ) {
+                throw new Error('Auth failed. Token missing or invalid format.');
+            }
+            // console.log(`\n token: ${token}\n`);
+            const decoded = jwt.verify(token, process.env.JWT_KEY)
+
+            if ( !userType.includes(decoded.userType) ) {
+                throw new Error(`Auth failed. User dosn't have required permissions.`);
+            }
+
+            req.userData = decoded;
+            next();
+
+        } catch ( error ) {
+            res.status(401).json({
+                message: `Auth failed. Invalide token or insufficient permissions.`
+            })
+        }
     }
-};
+}
+
+const checkCommonAuth = checkAuth(['common'])
+const checkAdminAuth = checkAuth(['admin'])
+const checkAllowAuth = checkAuth(['admin', 'common'])
+
+module.exports = {
+    checkCommonAuth,
+    checkAdminAuth,
+    checkAllowAuth
+}
