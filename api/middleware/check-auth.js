e7fd7d83d42475756b42cad27ced8a4f992e2c45
david <david.emanoel.706@ufrn.edu.br>
2024-01-17 12:33:15 -0300

    some validators

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -4,18 +4,18 @@ const checkAuth = (userType) => {
     return ( req, res, next ) => {
         try {
             const token = req.headers.authorization.split(' ')[1];
-
+            
             if ( !token ) {
                 throw new Error('Auth failed. Token missing or invalid format.');
             }
-            // console.log(`\n token: ${token}\n`);
+
             const decoded = jwt.verify(token, process.env.JWT_KEY)
 
             if ( !userType.includes(decoded.userType) ) {
                 throw new Error(`Auth failed. User doesn't have required permissions.`);
             }
 
-            if ( req.params.userId !== decoded.userId && decoded.userType === 'common'){
+            if ( req.params.userId && req.params.userId !== decoded.userId && decoded.userType === 'common'){
                 throw new Error(`Auth failed. Actions on other users are not allowed.`)
             }
 
