71c86ccd6f8cf4fc2de9366c2047c0aaad9b68cf
david <david.emanoel.706@ufrn.edu.br>
2024-01-18 16:07:33 -0300

    nodemailConfig

---
+++ src/config/nodemailConfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+    smtp_host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    myEmail: process.env.MYPASSEMAIL
+}
