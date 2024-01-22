602a14350ab177e1c46c4ac48c0b6a26e805661d
david <david.emanoel.706@ufrn.edu.br>
2024-01-22 15:59:17 -0300

    updates

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 
 const app = require('../app');
+const Test = require('supertest/lib/test');
 require('dotenv').config();
 
 
 beforeEach( async() => {
     try {
-        const dbUri = process.env.DATA_BASE
-        await mongoose.connect(dbUri);
+        await mongoose.connect( process.env.DATA_BASE );
         console.log('\n\t=> Connected!\n')
     } catch ( error ){
         console.log(`\nError: ${error}`)
@@ -16,9 +16,9 @@ beforeEach( async() => {
     }
 });
 
-describe('POST /v1/auth/signin/', () => {
+describe('Login', () => {
     try {
-        it( 'should return 200 if valid credentials', async () => {
+        test( 'should return 200 if valid credentials', async () => {
             return request(app)
                 .post("http://localhost:3000/v1/auth/signin")
                 .send({
@@ -39,6 +39,8 @@ describe('POST /v1/auth/signin/', () => {
 })
 
 afterEach(async() => {
+    await mongoose.connection.dropDatabase();
     await mongoose.connection.close();
+    await mongod.stop();
     console.log('Data base closed');
 });
