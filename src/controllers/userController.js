d78c8d8eb9012ff69a49ebfa8bcc00e2a0768d89
Davidemanoel7 <davidemanoel34@gmail.com>
2024-03-03 18:42:10 -0300

    change to trycatch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,242 +8,224 @@ const jwt = require('jsonwebtoken');
 
 const validationMidd = require('../middleware/validationMidw');
 
-exports.signup = [validationMidd.validate, (req, res, next) => {
-
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
+exports.signup = [validationMidd.validate, async (req, res, next) => {
+    try {
+        const hashedPass = bcrypt.hashSync( req.body.password, 10 );
+        if ( !hashedPass ) {
             return res.status(500).json({
-                error: err,
-            });
+                message: "Error while hashing your password. Try again!"
+            })
+        }
+        const user = new User({
+            _id: new mongoose.Types.ObjectId(),
+            name: req.body.name,
+            user: req.body.user,
+            email: req.body.email,
+            password: hashedPass,
+            userType: req.body.userType
+        });
+
+        await user.save();
+
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                user: user.user,
+                email: user.email,
+                userType: user.userType
+            },
+            process.env.JWT_KEY,
+            { expiresIn: '24h' }
+        );
+
+        res.status(201).json({
+            message: `Created user ${user.user} successfully`,
+            createdUser: {
+                id: user._id,
+                name: user.name,
+                user: user.user,
+                email: user.email,
+            },
+            token: token
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err,
+            message: `Cannot save this user... please try again :(`
+        });
+    }
+}];
+
+exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find().select('_id name user email').exec();
+
+        res.status(200).json({
+            count: users.length,
+            users: users
+        });
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        });
+    }
+}
+
+exports.getUser = async (req, res, next) => {
+    try {
+        const id = req.userData.userId
+        const user = await User.findOne({_id: id})
+            .select('_id name user email profileImage living')
+            .exec()
+
+        if ( user && user.living ) {
+            res.status(200).json(user);
         } else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
+            res.status(404).json({
+                message: "User not found or invalid"
+            })
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json( {error: err || 'Internal server error...'} )
+    }
+}
+
+exports.updateUser = [ validationMidd.validate, async (req, res, next) => {
+    try {
+        const id = req.userData.userId
+        const user = await User.findByIdAndUpdate( id ,
+            { $set: {
                 name: req.body.name,
                 user: req.body.user,
                 email: req.body.email,
-                password: hash,
-                userType: req.body.userType
-            });
-            user.save()
-                .then( result => {
-                    console.log(result);
-                    const token = jwt.sign(
-                        {
-                            userId: result._id,
-                            user: result.user,
-                            email: result.email,
-                            userType: result.userType
-                        
-                        },
-                        process.env.JWT_KEY,
-                        { expiresIn: '24h' }
-                    );
-                    res.status(201).json({
-                        message: `Created user ${result.user} successfully`,
-                        createdUser: {
-                            id: result._id,
-                            name: result.name,
-                            user: result.user,
-                            email: result.email,
-                            request: {
-                                type: "GET",
-                                url: `/v1/user/${result._id}/`,
-                            }
-                        },
-                        token: token
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({
-                        error: err,
-                        message: `Cannot save this user... please try again :(`
-                    });
-                });
-        }
-    });
-}];
+            }},
+            { new: true } 
+        );
 
-exports.getAllUsers = (req, res, next) => {
-    User.find()
-        .select('_id name user email')
-        .exec()
-        .then( docs => {
-            const response = {
-                count: docs.length,
-                users: docs
-            };
-            res.status(200).json(response);
-        })
-        .catch( err => {
-            console.log(err)
-            res.status(500).json({
-                error: err
+        if ( !user ) {
+            return res.status(404).json({
+                message: `User not found with ID ${id}`
             })
-        })
-}
-
-exports.getUser = (req, res, next) => {
-
-    const id = req.userData.userId
-
-    User.findOne({_id: id})
-        .select('_id name user email profileImage living')
-        .exec()
-        .then( doc => {
-            if ( doc && doc.living ) {
-                res.status(200).json(doc)
-            } else {
-                res.status(404).json({
-                    message: `User not found or invalid ${doc}`
-                })
-            }
-        })
-        .catch( err => {
-            console.log(err)
-            res.status(500).json( {error: err || 'Internal server error...'} )
-        })
-}
-
-exports.updateUser = [ validationMidd.validate, (req, res, next) => {
-
-    const id = req.userData.userId
-
-    User.findByIdAndUpdate( id,
-                    { $set: {
-                        name: req.body.name,
-                        user: req.body.user,
-                        email: req.body.email,
-                    }},
-                    { new: true })
-                    .then( result => {
-                        if (!result) {
-                            return res.status(404).json({
-                                message: `User not found with ID ${id}`
-                            })
-                        }
-                        res.status(200).json({
-                            message: `Updated user ${result.user} successfully`,
-                            updatedUser : result,
-                        })
-                    })
-                    .catch( err => res.status(500).json({
-                        error: `${err} Internal server error`
-                    }))
-}];
-
-exports.deleteUser = (req, res, next) => {
-    const id = req.userData.userId
-
-    User.findByIdAndDelete({_id: id})
-        .exec()
-        .then( result => {
-            if ( !result ) {
-                return res.status(404).json({
-                    message: `User by ID ${id} not found.`
-                })
-            }
-            Pass.deleteMany({ userId: result._id })
-                .then( usr => {
-                    console.log(usr);
-                    res.status(200).json({
-                        message: `User ${result.user} deleted successfully`,
-                    })
-                })
-                .catch( err => {
-                    console.log(err);
-                    throw err;
-                });
-        })
-        .catch( err => {
-            console.log(err)
-            res.status(500).json({
-                error: `${err} or Internal server error`
-            })
-        })
-}
-
-exports.inactivateUser = (req, res, next) => {
-    const id = req.userData.userId
-
-    User.findByIdAndUpdate(id,
-        { $set: { living: false }},
-        { new: true })
-        .then( usr => {
-            if (!usr) {
-                return res.status(404).json({
-                    message: `User by ID ${id} not found.`
-                })
-            }
-            res.status(200).json({
-                user: usr,
-                message: `User ${usr.name} inactivated successfully!`
-            })
-        })
-        .catch( err => {
-            console.log(err)
-            res.status(500).json({
-                error: err
-            })
-        })
-}
-
-exports.changeUserPass = [ validationMidd.validate, (req, res, next) => {
-    const id = req.userData.userId
-    const newPass = req.body.password
-
-    bcrypt.hash(newPass, 10, (err, hashed) => {
-        if (err) {
-            return res.status(500).json({
-                error: err
-            });
         }
 
-        User.findByIdAndUpdate( id,
-            { $set: { password: hashed }},
-            { new: true }
-            )
-            .then( result => {
-                if ( !result ) {
-                    return res.status(404).json({
-                        message: `User not found with ID ${id}`
-                    })
-                }
-                res.status(200).json({
-                    message: 'Password changed successfully!'
-                })
-            })
-            .catch( err => {
-                console.log(err)
-                res.status(500).json({
-                    error: `${err} Internal server error`
-                })
-            })
-    })
-}];
-
-exports.changeUserProfileImage = (req, res, next) => {
-    const id = req.userData.userId;
-
-    User.findByIdAndUpdate( id,
-        { $set: { profileImage: req.file.path }},
-        { new: true })
-        .then(result => {
-            if (!result) {
-                return res.status(404).json({
-                    message: `User not found with ID ${id}`
-                })
-            }
-            res.status(200).json({
-                message: `Profile image changed from user ${result.user}!`
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err,
-                message: `Cannot change profile image :(`
-            });
+        res.status(200).json({
+            message: `Updated user ${user.user} successfully`,
+            updatedUser : user,
         });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            error: `${err} Internal server error`
+        })
+    }
+}];
+
+exports.deleteUser = async (req, res, next) => {
+    try {
+        const id = req.userData.userId
+        const user = await User.findOneAndDelete({ _id: id }).exec();
+
+        if ( !user ) {
+            return res.status(404).json({
+                message: `User by ID ${id} not found.`
+            });
+        }
+
+        await Pass.deleteMany({ userId: id });
+
+        res.status(200).json({
+            message: `User ${user.user} deleted successfully`,
+        });
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            error: `${err} or Internal server error`
+        })
+    }
+}
+
+exports.inactivateUser = async (req, res, next) => {
+    try {
+        const id = req.userData.userId
+        const user = await User.findByIdAndUpdate( id, 
+            { $set: { living: false }},
+            { new: true }
+        );
+
+        if ( !user ) {
+            return res.status(404).json({
+                message: `User by ID ${id} not found.`
+            });
+        }
+
+        res.status(200).json({
+            user: user,
+            message: `User ${user.user} inactivated successfully!`
+        })
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })   
+    }
+}
+
+exports.changeUserPass = [ validationMidd.validate, async (req, res, next) => {
+    try {
+        const id = req.userData.userId
+        const newPass = bcrypt.hashSync( req.body.password, 10);
+
+        const user = await User.findByIdAndUpdate( id, 
+            { $set: { password: newPass }},
+            { new: true });
+        
+        if ( !user ) {
+            return res.status(404).json({
+                message: `User not found with ID ${id}`
+            });
+        }
+
+        res.status(200).json({
+            message: 'Password changed successfully!'
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            error: `${err} Internal server error`
+        })
+    }
+}];
+
+exports.changeUserProfileImage = async (req, res, next) => {
+    try {
+        const id = req.userData.userId;
+        const user = await User.findByIdAndUpdate( id,
+            { $set: { profileImage: req.file.path }},
+            { new: true });
+        
+        if ( !user ) {
+            return res.status(404).json({
+                message: `User not found with ID ${id}`
+            })
+        }
+
+        res.status(200).json({
+            message: `Profile image changed from user ${user.user}!`
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err,
+            message: `Cannot change profile image :(`
+        });        
+    }
 };
 
